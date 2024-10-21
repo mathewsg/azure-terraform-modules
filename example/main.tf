3015861df6c10d13eed22811d7c75a82e3e5568b
Mathews George <mathewsg6@gmail.com>
2024-10-21 11:51:13 +0000

    pushing changes in rg featuire

--- example/main.tf
+++ example/main.tf
@@ -5,23 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0, < 4.0.0"
     }
-    azapi = {
-      source = "Azure/azapi"
-    }
   }
 }
 
 provider "azurerm" {
   features {
-  }
-}
-
-
-module "resource_group" {
-  source   = "../"
-  name     = "test-rg"
-  location = "uksouth"
-  tags = {
-    env = "test"
   }
 }
